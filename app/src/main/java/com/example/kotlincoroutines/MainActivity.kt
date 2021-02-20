1d624b2033a4b805c2815d381d2766b83f3a2af1
mohamed tamer0 <mt4452242@gmail.com>
2021-02-20 21:54:13 +0200

    update

--- app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
@@ -2,13 +2,50 @@ package com.example.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        doTaskA(1)
     }
 
 
-    
+    private fun doTaskA(label:Int) {
+        when(label) {
+            1 -> {
+                Log.d("task A","we deal with one A")
+                doTaskB(1)
+            }
+            2 -> {
+                Log.d("task A","we deal with two A")
+                doTaskB(2)
+            }
+            3 -> {
+                Log.d("task A","we deal with three A")
+                doTaskB(3)
+            }
+        }
+    }
+
+    private fun doTaskB(label:Int) {
+        when(label) {
+            1 -> {
+                Log.d("task B","we deal with one B")
+                doTaskA(2)
+            }
+            2 -> {
+                Log.d("task B","we deal with two B")
+                doTaskA(3)
+            }
+            3 -> {
+                Log.d("task B","we deal with three B")
+            }
+        }
+    }
+
+
+
+
 }
