901bc5d278b778c245bcd24527ce1f760e719a87
mohamed tamer0 <mt4452242@gmail.com>
2021-02-20 22:32:26 +0200

    update

--- app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
@@ -3,21 +3,39 @@ package com.example.kotlincoroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         doTaskA(1)
-        printMyTextAfterDelay("Hello Kotlin")
+        printMyTextAfterDelay1("Hello Kotlin")
+
+        GlobalScope.launch {
+            Log.d("Fun", "Current thread : ${Thread.currentThread().name}")
+            printMyTextAfterDelay2("Coroutines")
+        }
+
     }
 
 
 
-    fun printMyTextAfterDelay(myText: String) {
+    fun printMyTextAfterDelay1(myText: String) {
         Thread.sleep(2000)
         Log.d("my Fun", myText)
     }
+
+    //Coroutines
+    suspend fun printMyTextAfterDelay2(myText: String) {
+        delay(2000)
+        Log.d("my Fun", myText)
+    }
+
+
+
 
 
 
